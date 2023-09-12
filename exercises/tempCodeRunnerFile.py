d115bdefc5a95e3751ee4a7c6e28f52701191207
danisa <danisagomez@gmail.com>
2023-09-12 20:22:11 -0300

    resolucion6

---
+++ exercises/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+Convertir todo los numeros a enteros y luego restarlos secuencialmente (El uno
+menos el dos menos el tres menos el cuatro).
+"""
+
+numero_01 = "987"
+numero_02 = "0x6f54F"
+numero_03 = "0o1234"
+numero_04 = 654
